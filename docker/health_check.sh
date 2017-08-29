f7cc112abfb2ea4cae338f550b4635f7f99dc5b3
wujingtao <601595686@qq.com>
2017-08-30 02:35:40 +0800

    修复健康检查脚本的url问题

--- docker/health_check.sh
+++ docker/health_check.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 查询健康检查结果
-result=$(curl -s --no-buffer -XGET --unix-socket /tmp/service_starter_health_checking.sock http:// || echo 1)
+result=$(curl -s --no-buffer -XGET --unix-socket /tmp/service_starter_health_checking.sock http://1 || echo 1)
 
 if [ "$result" = "0" ]; then
     exit 0
