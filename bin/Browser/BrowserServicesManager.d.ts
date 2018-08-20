7fe1c3ef87a21a5024c3804bb8cbdfe43db54d74
wujingtao <601595686@qq.com>
2018-08-21 00:36:15 +0800

    优化了一下项目结构

--- bin/Browser/BrowserServicesManager.d.ts
+++
@@ -1,8 +0,0 @@
-import { BrowserServicesManagerConfig } from './BrowserServicesManagerConfig';
-import { BaseServicesManager } from './../common/BaseServicesManager';
-import { BaseServiceModule } from '../common/BaseServiceModule';
-export declare class BrowserServicesManager extends BaseServicesManager {
-    private readonly _config;
-    constructor(_config?: BrowserServicesManagerConfig);
-    onError(errName: string | undefined, err: Error, service: BaseServiceModule): void;
-}
--- bin/Docker/DockerServicesManagerConfig.d.ts
+++
@@ -1,8 +0,0 @@
-import { NodeServicesManagerConfig } from '../Node/NodeServicesManagerConfig';
-export interface DockerServicesManagerConfig extends NodeServicesManagerConfig {
-    /**
-     * 是否启动Docker健康检查服务器(默认true,启动)
-     * 注意：只有在Linux下才起作用
-     */
-    startHealthChecking?: boolean;
-}
