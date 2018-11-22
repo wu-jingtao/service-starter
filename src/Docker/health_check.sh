beec027e996189733e1abd822034eb6fa171ec82
wujingtao <601595686@qq.com>
2018-11-23 01:05:00 +0800

    更新了健康检查脚本

--- src/Docker/health_check.sh
+++ src/Docker/health_check.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 查询健康检查结果
-result=$(curl --connect-timeout 30 --max-time 30 -s --no-buffer -XGET --unix-socket /tmp/service_starter_health_checking.sock http://)
+result=$(curl --connect-timeout 30 --max-time 30 -s --no-buffer -XGET --unix-socket /tmp/service_starter_health_checking.sock http://health-check.com)
 echo $result
 
 if [ "$result" = "running" -o "$result" = "stopped" -o "$result" = "starting" -o "$result" = "stopping" ]; then
